0e45b10d88708f6ec538ad070eb9713a4d39d986
Alekzum <68947902+Alekzum@users.noreply.github.com>
2025-11-10 22:35:02 +0400

    all required config's vars with type str

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
-IMGBB_API_KEY = os.getenv("IMGBB_API_KEY")
+BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
+OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY") or ""
+IMGBB_API_KEY = os.getenv("IMGBB_API_KEY") or ""
 
 THREAD_POOL_WORKERS = int(os.getenv("THREAD_POOL_WORKERS", "4"))
 REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "4"))
@@ -15,4 +15,6 @@ CACHE_TTL = int(os.getenv("CACHE_TTL", "600"))
 required_vars = ["BOT_TOKEN", "OPENWEATHERMAP_API_KEY", "IMGBB_API_KEY"]
 for var in required_vars:
     if not globals()[var]:
-        raise ValueError(f"Отсутствует обязательная переменная окружения: {var}")
+        raise ValueError(
+            f"Отсутствует обязательная переменная окружения: {var}"
+        )
