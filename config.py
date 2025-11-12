6e739db8413f78f25b7b0e706a9585e07a3411d6
Alekzum <68947902+Alekzum@users.noreply.github.com>
2025-11-12 22:48:43 +0400

    add openweathermap's default base url

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN") or ""
 OPENWEATHERMAP_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY") or ""
 IMGBB_API_KEY = os.getenv("IMGBB_API_KEY") or ""
-OPENWEATHERMAP_BASE_URL = os.getenv("OPENWEATHERMAP_BASE_URL") or ""
 
+OPENWEATHERMAP_BASE_URL = os.getenv("OPENWEATHERMAP_BASE_URL") or "https://api.openweathermap.org"
 THREAD_POOL_WORKERS = int(os.getenv("THREAD_POOL_WORKERS", "4"))
 REQUEST_TIMEOUT = int(os.getenv("REQUEST_TIMEOUT", "4"))
 UPLOAD_TIMEOUT = int(os.getenv("UPLOAD_TIMEOUT", "3"))
